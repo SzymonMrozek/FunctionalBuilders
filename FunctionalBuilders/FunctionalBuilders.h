adb74ae1d17fcd0a8075c3ae8274789b182cb425
SzymonMrozek <szymon.mrozek.sm@gmail.com>
2018-03-07 21:18:51 +0100

    Initial commit

---
+++ FunctionalBuilders/FunctionalBuilders.h
@@ -0,0 +1,19 @@
+//
+//  FunctionalBuilders.h
+//  FunctionalBuilders
+//
+//  Created by Szymon Mrozek on 07.03.2018.
+//  Copyright © 2018 SzymonMrozek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FunctionalBuilders.
+FOUNDATION_EXPORT double FunctionalBuildersVersionNumber;
+
+//! Project version string for FunctionalBuilders.
+FOUNDATION_EXPORT const unsigned char FunctionalBuildersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunctionalBuilders/PublicHeader.h>
+
+
